bcf2bc0df8bed5cce69898731009ad3e290a1e0b
fengxueem <fengxueem@gmail.com>
2017-05-12 07:59:47 +0800

    Word class: 1. make 'name' final 2. add an explicit lock for future multithread purpose 3. add a new method 'addCount(int count)' to manipulate occurrenceCount Update UML plan according to the changes above

--- source/Word.java
+++ source/Word.java
@@ -1,6 +1,9 @@
+import java.util.concurrent.locks.*;
+
 public class Word implements Comparable<Word> {
-  private String name;
+  private final String name;
   private int occurrenceCount;
+  private Lock lock = new ReentrantLock();
 
   public Word(String name) {
   	this.name = name;
@@ -17,6 +20,20 @@ public class Word implements Comparable<Word> {
   }
 
   public int getOccurrenceCount() {
-  	return occurrenceCount;
+  	lock.lock();
+  	try {
+  	  return occurrenceCount;
+  	} finally {
+  	  lock.unlock();
+  	}
+  }
+
+  public void addCount(int count) {
+  	lock.lock();
+  	try {
+  	  occurrenceCount += count;
+  	} finally {
+  	  lock.unlock();
+  	}
   }
 }
