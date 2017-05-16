4619f852ddfcc7fd13c4f7a7b0c966568f797f89
fengxueem <fengxueem@gmail.com>
2017-05-16 16:01:39 +0800

    *Urgent fix: add an getIterator() method for WordRepo

--- source/SortedWordRepo.java
+++ source/SortedWordRepo.java
@@ -25,6 +25,9 @@ public class SortedWordRepo implements WordRepo {
   	}
   	return null;
   }
+  public Iterator<Word> getIterator() {
+    return allWords.iterator();
+  }
   public String toString() {
   	return allWords.toString();
   }
--- source/WordRepo.java
+++ source/WordRepo.java
@@ -1,5 +1,8 @@
+import java.util.Iterator;
+
 interface WordRepo {
   void put(Word w);
   boolean remove(Word w);
   Word get(String wordName);
+  Iterator<Word> getIterator();
 }
