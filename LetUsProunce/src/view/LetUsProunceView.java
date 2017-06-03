3e77eddfab46de25dcc77176f6a4bb948de95df2
fengxueem <fengxueem@gmail.com>
2017-06-03 17:18:07 +0800

    Set menu bar as: File, Word Repo, Dictate

--- LetUsProunce/src/view/LetUsProunceView.java
+++ LetUsProunce/src/view/LetUsProunceView.java
@@ -15,10 +15,12 @@ import javax.swing.JTextField;
 public class LetUsProunceView extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textField;
+	private JMenuBar menuBar;
+	public JMenu mnFile, mnWordRepo, mnDictate;
+	public JMenuItem mntmFile;
 
 	/**
-	 * Launch the application.
+	 * Launch the view.
 	 */
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
@@ -40,32 +42,26 @@ public class LetUsProunceView extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		
-		JMenuBar menuBar = new JMenuBar();
+		// create menu bar
+		menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
 		
-		JMenu mnFile = new JMenu("File");
+		mnFile = new JMenu("File");
 		menuBar.add(mnFile);
 		
-		JMenuItem mntmFile = new JMenuItem("Open File...");
+		mntmFile = new JMenuItem("Open File...");
 		mnFile.add(mntmFile);
 		
-		JMenu mnWordRepo = new JMenu("Word Repo");
+		mnWordRepo = new JMenu("Word Repo");
 		menuBar.add(mnWordRepo);
 		
-		JMenu mnDictate = new JMenu("Dictate");
+		mnDictate = new JMenu("Dictate");
 		menuBar.add(mnDictate);
+		
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 		
-		JScrollBar scrollBar = new JScrollBar();
-		contentPane.add(scrollBar, BorderLayout.CENTER);
 		
-		textField = new JTextField();
-		textField.setText("1");
-		contentPane.add(textField, BorderLayout.NORTH);
-		textField.setColumns(10);
 	}
 
 }
